bd0545a355f108f98283b84b0a79a1fa6d65b8e7
Ben Schwarz <ben@germanforblack.com>
2013-03-14 22:28:49 +1100

    Added prefixed build option - Simplified sass build

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,24 +3,20 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
     watch: {
       files: ['sass/*.scss', 'index.html'],
       tasks: ['sass:dev']
     },
     sass: {
       dist: {
-        files: {
-          'css/gallery.build.css': 'sass/gallery.build.scss',
-          'css/gallery.theme.css': 'sass/gallery.theme.scss'
-        }
-      },
-      dev: {
         options: {
           style: 'expanded'
         },
         files: {
-          'css/gallery.build.css': 'sass/gallery.build.scss',
-          'css/gallery.theme.css': 'sass/gallery.theme.scss'
+          'css/<%= pkg.name %>.css': 'sass/<%= pkg.name %>.build.scss',
+          'css/<%= pkg.name %>.theme.css': 'sass/<%= pkg.name %>.theme.scss'
         }
       }
     },
@@ -29,19 +25,36 @@ module.exports = function(grunt) {
         src: ['css/*.css']
       }
     },
+    rework: {
+      'css/<%= pkg.name %>.prefixed.css': 'css/<%= pkg.name %>.css',
+      options: {
+        use: [
+          ['rework.keyframes'],
+          ['rework.prefix', 'animation'],
+          ['rework.prefix', 'animation-delay'],
+          ['rework.prefix', 'transition']
+        ],
+        vendors: ['-moz-', '-webkit-', '-o-']
+      }
+    },
     cssmin: {
-      dist: {
+      unprefixed: {
         src: 'css/gallery.css',
         dest: 'css/gallery.min.css'
+      },
+      prefixed: {
+        src: 'css/gallery.prefixed.css',
+        dest: 'css/gallery.prefixed.min.css'
       }
     }
   });
 
   // Default task.
-  grunt.registerTask('default', ['sass', 'csslint', 'cssmin']);
+  grunt.registerTask('default', ['sass', 'rework', 'csslint', 'cssmin']);
 
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-rework');
 };
